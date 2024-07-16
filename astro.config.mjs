670761c915065772900650d5f1e257cf88401b59
Abhigyan Tripathi <abhigyantrips@gmail.com>
2024-07-16 15:24:44 +0530

    feat(meta): add tailwind and mdx integrations

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,23 @@
 import { defineConfig } from 'astro/config';
 
+import tailwind from "@astrojs/tailwind";
+import mdx from '@astrojs/mdx';
+
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://www.abhigyantrips.dev',
-	redirects: {
-		'/discord': 'https://discord.gg/zdVnRaQVzn'
+  site: 'https://www.abhigyantrips.dev',
+  redirects: {
+    '/discord': 'https://discord.gg/zdVnRaQVzn'
+  },
+  integrations: [tailwind(), mdx(
+	{
+		syntaxHighlight: 'shiki',
+		shikiConfig: {
+			themes: {
+				light: 'rose-pine-dawn',
+				dark: 'vitesse-dark'
+			}
+		}
 	}
+  )]
 });
