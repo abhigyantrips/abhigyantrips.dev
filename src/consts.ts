c014596e59b56ae2b64278b3c75b8ae645d0fe51
Abhigyan Tripathi <abhigyantrips@gmail.com>
2024-07-18 16:07:25 +0530

    feat(meta): add BaseHead

---
+++ src/consts.ts
@@ -0,0 +1,24 @@
+import type { Site, Socials } from "@/types";
+
+// Global
+export const SITE: Site = {
+  TITLE: 'Abhigyan Trips',
+  DESCRIPTION: 'Currently working towards a distraction-free internet.',
+  AUTHOR: 'Abhigyan Tripathi'
+}
+
+// Socials
+export const SOCIALS: Socials = [
+  {
+    NAME: 'GitHub',
+    ICON: 'github',
+    TEXT: 'abhigyantrips',
+    HREF: 'https://github.com/abhigyantrips'
+  },
+  {
+    NAME: "LinkedIn",
+    ICON: "linkedin",
+    TEXT: 'abhigyantrips',
+    HREF: 'https://linkedin.com/in/abhigyantrips'
+  }
+]
---
+++ src/types.ts
@@ -0,0 +1,15 @@
+export type Page = {
+  TITLE: string;
+  DESCRIPTION: string;
+};
+
+export interface Site extends Page {
+  AUTHOR: string;
+}
+
+export type Socials = {
+  NAME: string;
+  ICON: string;
+  TEXT: string;
+  HREF: string;
+}[];
