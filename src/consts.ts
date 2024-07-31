f34d34c7ebd4328d2e6e16d89937a7f8103a2618
Abhigyan Tripathi <abhigyantrips@gmail.com>
2024-07-31 14:30:52 +0530

    feat(content): add styling to labs and blogs

--- src/consts.ts
+++ src/consts.ts
@@ -1,24 +1,48 @@
-import type { Site, Socials } from "@/types";
+import type { Page, Site, Socials } from "@/types";
 
 // Global
 export const SITE: Site = {
-  TITLE: 'Abhigyan Trips',
-  DESCRIPTION: 'Currently working towards a distraction-free internet.',
-  AUTHOR: 'Abhigyan Tripathi'
+	TITLE: 'Abhigyan Trips',
+	DESCRIPTION: 'Currently working towards a distraction-free internet.',
+	AUTHOR: 'Abhigyan Tripathi'
+}
+
+// Blog
+export const BLOG: Page = {
+	TITLE: 'Blog',
+	DESCRIPTION: 'Journeys, experiences and rants, all in one place.'
+}
+
+// Labs
+export const LABS: Page = {
+	TITLE: 'Labs',
+	DESCRIPTION: 'Source code of all my lab practicals, for people to "inspire" from.'
+}
+
+// Projects
+export const PROJECTS: Page = {
+	TITLE: 'Projects',
+	DESCRIPTION: 'A list of projects I\'ve developed, for myself and other organizations.'
+}
+
+// Tags
+export const TAGS: Page = {
+	TITLE: 'Tags',
+	DESCRIPTION: 'All blogs, projects and labs categorized by their tags.'
 }
 
 // Socials
 export const SOCIALS: Socials = [
-  {
-    NAME: 'GitHub',
-    ICON: 'github',
-    TEXT: 'abhigyantrips',
-    HREF: 'https://github.com/abhigyantrips'
-  },
-  {
-    NAME: "LinkedIn",
-    ICON: "linkedin",
-    TEXT: 'abhigyantrips',
-    HREF: 'https://linkedin.com/in/abhigyantrips'
-  }
+	{
+		NAME: 'GitHub',
+		ICON: 'github',
+		TEXT: 'abhigyantrips',
+		HREF: 'https://github.com/abhigyantrips'
+	},
+	{
+		NAME: "LinkedIn",
+		ICON: "linkedin",
+		TEXT: 'abhigyantrips',
+		HREF: 'https://linkedin.com/in/abhigyantrips'
+	}
 ]
